6591b4ea86d015c34540c68c9e3415467139b3f7
Varunesh Pathak <varuneshpathak21@gmail.com>
2024-12-04 23:07:08 +0530

    Login and register form completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from './pages/HomePage.js';
+import Login from './pages/Login.js';
+import Register from './pages/Register.js';
 
 function App() {
   return (
-    <div className="container-fluid">
-      <h1 className='text-center'>Hello</h1>
-    </div>
+    <>
+      <Routes>
+        <Route 
+          path='/' 
+          element={
+            <ProtectedRoutes>
+              <HomePage />
+            </ProtectedRoutes>
+          } 
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
+}
+
+export function ProtectedRoutes(props){
+  if(localStorage.getItem('user')){
+    return props.children;
+  } else {
+    return <Navigate to="/login" />;
+  }
 }
 
 export default App;
